0db9f9d8affcced63dedcd50181eda8f12b2a970
Artem721 <71816794+Artem721@users.noreply.github.com>
2020-11-05 10:19:11 +0300

    Add files via upload

--- ft_rshift_list.py
+++ ft_rshift_list.py
@@ -7,9 +7,12 @@ def ft_len(v):
 
 def ft_rshift_list(num):
     w = ft_len(num)
+    w = w - 1
     q = []
+    i = 1
     q.append(num[w - 1])
     w = w - 1
     for i in range(w):
         q.append(num[i])
+        i = i + 1
     return q
