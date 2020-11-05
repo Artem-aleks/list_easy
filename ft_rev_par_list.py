03ed6c4c041475b562383f2cb201b633c625df0e
Artem721 <71816794+Artem721@users.noreply.github.com>
2020-11-05 10:16:03 +0300

    Add files via upload

--- ft_rev_par_list.py
+++ ft_rev_par_list.py
@@ -8,13 +8,13 @@ def ft_len(v):
 def ft_rev_par_list(num):
     if ft_len(num) % 2 == 0:
         i = 0
-    while i != ft_len(num):
-        num[i], num[i + 1] = num[i + 1], num[i]
-        i += 2
+        while i != ft_len(num):
+            num[i], num[i + 1] = num[i + 1], num[i]
+            i += 2
         return num
     else:
         i = 0
         while i != ft_len(num) - 1:
             num[i], num[i + 1] = num[i + 1], num[i]
-        i += 2
+            i += 2
         return num
