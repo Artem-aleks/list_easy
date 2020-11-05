fd830e6ece0acc1a921bb048f42d39d91bd24e79
Artem721 <71816794+Artem721@users.noreply.github.com>
2020-11-05 09:56:11 +0300

    Add files via upload

--- ft_same_parts_list.py
+++ ft_same_parts_list.py
@@ -9,6 +9,6 @@ def ft_same_parts_list(num):
     i = 1
     while i != ft_len(num):
         if (num[i - 1] > 0 and num[i] > 0) or (num[i - 1] < 0 and num[i] < 0):
-            i += i
-    return True
+            i += 1
+            return True
     return False
